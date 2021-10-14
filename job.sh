c298947a5bcb2a31ad36824d029ddebb87aa855a
Skipcast <jonathanjl99@gmail.com>
2021-10-14 17:05:38 +0200

    Added more inputs

--- job.sh
+++ job.sh
@@ -25,6 +25,13 @@ add_arg_if_not_empty "-betapassword" "$INPUT_BETA_PASSWORD"
 add_arg_if_not_empty "-osarch" "$INPUT_OSARCH"
 add_arg_if_not_empty "-loginid" "$INPUT_LOGINID"
 add_arg_if_not_empty "-language" "$INPUT_LANGUAGE"
+add_arg_if_not_empty "-max-downloads" "$INPUT_MAXDOWNLOADS"
+add_arg_if_not_empty "-max-servers" "$INPUT_MAXSERVERS"
+add_arg_if_not_empty "-cellid" "$INPUT_CELLID"
+add_arg_if_not_empty "-pubfile" "$INPUT_PUBFILE"
+add_arg_if_not_empty "-ugc" "$INPUT_UGC"
+add_arg_if_not_empty "-manifest" "$INPUT_MANIFEST"
+add_arg_if_not_empty "-depot" "$INPUT_DEPOT"
 
 # Create filelist if input files are specified and add -filelist argument
 if [ "$INPUT_FILES" ]; then
